ba6a1d62fe853a61919d8ce9731995115c6faa82
Cheng <chengjohnsonhs@gmail.com>
2020-01-22 04:06:09 +0800

    Move styles out and add constants file

---
+++ src/styles/ColorPickerFormStyles.js
@@ -0,0 +1,16 @@
+export default {
+  picker: {
+    width: "100% !important",
+    marginTop: "1rem"
+  },
+  addColor: {
+    width: "100%",
+    padding: "0.5rem",
+    marginTop: "0.5rem",
+    fontSize: "1rem"
+  },
+  colorNameInput: {
+    width: "100%",
+    height: "70px"
+  }
+};
