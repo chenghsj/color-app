b93d684f58db69e1d0d407b5bdc7799d0d505277
Cheng <chengjohnsonhs@gmail.com>
2020-01-26 12:34:55 +0800

    Prevent duplicate random colors

--- src/App.js
+++ src/App.js
@@ -102,6 +102,17 @@ class App extends Component {
                     </Page>
                   )}
                 />
+                <Route
+                  render={routeProps => (
+                    <Page>
+                      <PaletteList
+                        {...routeProps}
+                        palettes={this.state.palettes}
+                        deletePalette={this.deletePalette}
+                      />
+                    </Page>
+                  )}
+                />
               </Switch>
             </CSSTransition>
           </TransitionGroup>
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -74,8 +74,16 @@ class NewPaletteForm extends Component {
 
   addRandomColor() {
     const allColors = this.props.palettes.map(p => p.colors).flat();
-    var rand = Math.floor(Math.random() * allColors.length);
-    const randomColor = allColors[rand];
+    let rand;
+    let randomColor;
+    let isDuplicateColor = true;
+    const checkDuplicate = () =>
+      this.state.colors.some(color => color.name === randomColor.name);
+    while (isDuplicateColor) {
+      rand = Math.floor(Math.random() * allColors.length);
+      randomColor = allColors[rand];
+      isDuplicateColor = checkDuplicate();
+    }
     this.setState({ colors: [...this.state.colors, randomColor] });
   }
 
